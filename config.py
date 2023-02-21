714f45ebfc3d49b808531c197af829bf251ed69f
sktut <64089081+sktut@users.noreply.github.com>
2023-02-21 23:03:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+MAX_DEPTH = 10  
+MIN_DEPTH = 3 
+MAX_WAIT = 10  
+MIN_WAIT = 5  
+DEBUG = True 
+
+ROOT_URLS = [
+"https://techbeastexplorer.blogspot.com/",
+"https://techbeastexplorer.blogspot.com/2020/04/canon-latest-news.html"
+
+]
+# items can be a URL "https://www.anything(for eg sandeep).com" where you want to get views
+blacklist = [
+	 
+	]  
+
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' \
+	'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+# here the user agent must be a good application.
